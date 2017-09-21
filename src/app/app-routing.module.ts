f556e7c118a823c939afd76bcb2f67c71caac9dd
Mike Dacanay <migd64@gmail.com>
2017-09-20 18:30:02 -0700

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { SanJoseComponent } from './san-jose/san-jose.component';
+import { SeattleComponent } from './seattle/seattle.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{ path: '', pathMatch: 'full', component: SanJoseComponent },
-	{ path: 'sanjose', pathMatch: 'full', component: SanJoseComponent }
+	{ path: 'sanjose', pathMatch: 'full', component: SanJoseComponent },
+	{ path: 'seattle', pathMatch: 'full', component: SeattleComponent }
 ];
 
 @NgModule({
--- src/app/seattle/seattle.component.ts
+++ src/app/seattle/seattle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ThreadService } from './../thread/thread.service'
 @Component({
   selector: 'app-seattle',
   templateUrl: './seattle.component.html',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeattleComponent implements OnInit {
 
-  constructor() { }
+  weather = {};
+  constructor(private _threadService: ThreadService) {
+  	this.todayWeather();
+  }	
+
+  todayWeather(){
+  	this._threadService.getWeather((tasks) => {
+    	this.weather = tasks;
+    },"https://api.openweathermap.org/data/2.5/weather?zip=98004,us&APPID=d5ab84f3bbff04a8a02a7d894914bc55");
+  }
+
 
   ngOnInit() {
   }
